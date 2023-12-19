cc0535ab907b58f67e7cdb58f6f40e4a77f2f550
L1nda-H <linda.ly.hu@gmail.com>
2023-12-19 15:35:14 -0700

    Finish part 5 of tutorial

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,16 @@
  */
 module.exports = {
   siteMetadata: {
+    title: "A website",
     siteUrl: `https://www.yourdomain.tld`,
   },
   plugins: ["gatsby-plugin-image",
-    "gatsby-plugin-sharp",],
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blarg`,
+        path: `${__dirname}/blarg`,
+      }
+    },],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
 import * as React from 'react'
 import Layout from '../components/layout'
 import { StaticImage } from 'gatsby-plugin-image'
+import Seo from '../components/seo'
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
-      <p>children</p>
+      <p>yippee</p>
       <StaticImage
         alt='My cat Cocoa'
         src="../images/IMG_1353.jpg" />
@@ -13,6 +14,6 @@ const IndexPage = () => {
   )
 }
 
-export const Head = () => <title>Home Page</title>
+export const Head = () => <Seo title="Home Page" />
 
 export default IndexPage
