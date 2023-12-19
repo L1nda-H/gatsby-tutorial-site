750cf4fca024546b45f452e9d98fab4fd7a1d84d
L1nda-H <linda.ly.hu@gmail.com>
2023-12-19 11:03:53 -0700

    Complete step 3 of tutorial
    - used static image plug in to insert image of cat

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,14 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>children</p>
+      <StaticImage
+        alt='My cat Cocoa'
+        src="../images/IMG_1353.jpg" />
     </Layout>
   )
 }
